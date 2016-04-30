2e9719a8234bfebbe2b5164c73a2a0b7ba726538
Luiz Guilherme <luizguilherme@recebedor.com>
2016-04-30 18:56:26 -0300

    Adding local config check

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
 var configGlobal = require('./global');
-var configLocal = require('./local');
-
+try {
+	var configLocal = require('./local');
+} catch (e) {
+    var configLocal = {};
+}
 function mergeObjects(obj1,obj2){
     var obj3 = {};
     for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
